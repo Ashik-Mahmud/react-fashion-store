d42ad35f8d2d4addef20942fe627ba9621799a74
ashikmahmud <ashikmamud187@gmail.com>
2022-03-18 02:06:06 +0600

    added new features for responsive purpose and change app title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,20 +6,22 @@ export default function Header({ cart }) {
       <header id="header">
         <nav className="navbar py-4 bg-light navbar-expand-lg navbar-light">
           <div className="container">
-            <div className="logo fw-bold">Logo</div>
+            <div className="logo fw-bold h3 text-uppercase">
+              Fash<span className="text-danger">ion</span>
+            </div>
             <div className="primary-menus">
               <ul className="navbar-nav">
-                <li className="nav-item">
+                <li className="nav-item  d-none d-sm-block d-md-block d-lg-block">
                   <a href="/home" className="nav-link">
                     Home
                   </a>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item  d-none d-sm-block d-md-block d-lg-block">
                   <a href="/products" className="nav-link">
                     Products
                   </a>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item  d-none d-sm-block d-md-block d-lg-block">
                   <a href="/contact" className="nav-link">
                     Contact
                   </a>
