75e2fc16d266482662d22f86280892c9118c0c15
ashikmahmud <ashikmamud187@gmail.com>
2022-03-18 01:58:41 +0600

    Added new features called animation with spinner before loadded all of products

--- src/components/ProductBody/ProductBody.jsx
+++ src/components/ProductBody/ProductBody.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import { ProductCard } from "../ProductCard/ProductCard";
 export default function ProductBody({ setCart, cart }) {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products`)
       .then((response) => response.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        setProducts(data);
+        setLoading(true);
+      });
   }, []);
 
   return (
@@ -14,14 +19,21 @@ export default function ProductBody({ setCart, cart }) {
       <div className="container py-4">
         <h1>Fashion Products</h1>
         <div className="row my-3 g-4">
-          {products.map((product) => (
-            <ProductCard
-              key={product.id}
-              cart={cart}
-              setCart={setCart}
-              product={product}
-            />
-          ))}
+          {loading ? (
+            products.map((product) => (
+              <ProductCard
+                key={product.id}
+                cart={cart}
+                setCart={setCart}
+                product={product}
+              />
+            ))
+          ) : (
+            <div className="position-absolute w-75 my-5 py-5 text-center">
+              <Spinner animation="border" />
+            </div>
+          )}
+          {}
         </div>
       </div>
     </div>
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import { Fade } from "react-reveal";
 import DetailsModal from "../Modal/DetailsModal";
-
 export const ProductCard = ({ product, setCart, cart }) => {
   const { title, image, price, rating } = product;
 
@@ -12,43 +12,45 @@ export const ProductCard = ({ product, setCart, cart }) => {
 
   return (
     <div className="col-lg-3">
-      <div className="card border-0 shadow p-3">
-        <div className="card-header border-0 text-center bg-light p-3">
-          <img width={"200"} height={"200"} src={image} alt={title} />
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">
-            {title.length > 40 ? title.slice(0, 40) + "..." : title}
-          </h5>
+      <Fade up duration={1000} distance={"50px"}>
+        <div className="card border-0 shadow p-3">
+          <div className="card-header border-0 text-center bg-light p-3">
+            <img width={"200"} height={"200"} src={image} alt={title} />
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">
+              {title.length > 40 ? title.slice(0, 40) + "..." : title}
+            </h5>
 
-          <div className="my-3 d-flex align-items-center justify-content-between">
-            <p className="m-0 text-success fw-bold">
-              <span>{price}$</span>
-            </p>
-            <span className="text-danger fw-bold d-flex align-items-center">
-              {stars.map((number) => (
-                <AiFillStar key={number} />
-              ))}
-              {rating.rate}
-              <span className="text-dark small text-muted ms-2">
-                ({rating.count}){" "}
+            <div className="my-3 d-flex align-items-center justify-content-between">
+              <p className="m-0 text-success fw-bold">
+                <span>{price}$</span>
+              </p>
+              <span className="text-danger fw-bold d-flex align-items-center">
+                {stars.map((number) => (
+                  <AiFillStar key={number} />
+                ))}
+                {rating.rate}
+                <span className="text-dark small text-muted ms-2">
+                  ({rating.count}){" "}
+                </span>
               </span>
-            </span>
-          </div>
-          <div className="d-flex gap-2 mt-2">
-            <button
-              onClick={(e) => {
-                e.target.style = "opacity: 0.6; pointer-events:none;";
-                setCart(cart + 1);
-              }}
-              className="btn btn-sm btn-primary rounded-0"
-            >
-              Add to Cart
-            </button>
-            <DetailsModal product={product} />
+            </div>
+            <div className="d-flex gap-2 mt-2">
+              <button
+                onClick={(e) => {
+                  e.target.style = "opacity: 0.6; pointer-events:none;";
+                  setCart(cart + 1);
+                }}
+                className="btn btn-sm btn-primary rounded-0"
+              >
+                Add to Cart
+              </button>
+              <DetailsModal product={product} />
+            </div>
           </div>
         </div>
-      </div>
+      </Fade>
     </div>
   );
 };
